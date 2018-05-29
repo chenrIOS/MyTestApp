2402f71e89239dad03fa456fb16e773fb6530901
chenrui <chenrui@boxiao.cn>
2018-05-29 15:25:17 +0800

    Initial Commit

---
+++ MyTestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTestApp
+//
+//  Created by chenrui on 2018/5/29.
+//  Copyright © 2018年 博校. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyTestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyTestApp
+//
+//  Created by chenrui on 2018/5/29.
+//  Copyright © 2018年 博校. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
